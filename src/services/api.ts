9887e8990d76332e7211d3e00f805d1c24dd2acf
Robert Kelley <robertkelley1987@gmail.com>
2024-05-17 11:34:14 -0400

    Updated api to use EC2 location of server.

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,8 @@
 import axios from "axios";
 
-let baseURL = "/";
-// if (process.env.NODE_ENV === "production") {
-//   baseURL = "https://ec2-3-80-48-3.compute-1.amazonaws.com";
-// }
+let baseURL = "http://ec2-18-191-228-177.us-east-2.compute.amazonaws.com/";
 
 export const api = axios.create({
   baseURL,
-  // withCredentials: true
+  withCredentials: true,
 });
